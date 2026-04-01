ced0b7b774958b1c5b658d65dea8469f66abd753
Seitaro <tahara.seitaro@gmail.com>
2026-04-01 22:49:15 +0900

    Remove custom font dependencies

--- apps/spexor/src/main.tsx
+++ apps/spexor/src/main.tsx
@@ -1,7 +1,3 @@
-import "@fontsource/ibm-plex-mono/400.css";
-import "@fontsource/ibm-plex-sans/400.css";
-import "@fontsource/ibm-plex-sans/500.css";
-import "@fontsource/ibm-plex-sans/600.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
